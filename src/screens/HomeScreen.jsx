3636b484a6697b02ddf73c3f862b54fb743d89e2
darlingson <dmakuwila@gmail.com>
2024-04-18 23:29:02 +0200

    added code to retrive images

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { ScrollView, View, Text, Image, Dimensions } from 'react-native';
 import { Button, Card } from 'react-native-paper';
-import Carousel from 'react-native-snap-carousel';
 import jsonData from '../../assets/data.json';
+import img from '../../assets/images/image1.jpg';
 
 const windowWidth = Dimensions.get('window').width;
 
@@ -18,28 +18,21 @@ const HomePage = () => {
     setDestinations(shuffled.slice(0, 5));
   };
 
-  const renderCarouselItem = ({ item }) => {
-    return (
-      <Image
-        source={{ uri: item }}
-        style={{ width: windowWidth - 40, height: 200, borderRadius: 8 }}
-        resizeMode="cover"
-      />
-    );
-  };
-
   return (
     <ScrollView contentContainerStyle={{ padding: 20 }}>
       {destinations.map((destination, index) => (
         <Card key={destination.id}>
           <Card.Title title={destination.name} />
-          {console.log(destination.imagePaths)}
-          <Carousel
-            data={destination.imagePaths} // Using imagePaths array from each destination
-            renderItem={renderCarouselItem}
-            sliderWidth={windowWidth}
-            itemWidth={windowWidth}
-          />
+          <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+            {destination.imagePaths.map((imagePath, index) => (
+              <Image
+                key={index}
+                style={{ width: windowWidth / 2 - 30, height: 100, margin: 5, borderRadius: 8 }}
+                source={img }
+                resizeMode="cover"
+              />
+            ))}
+          </View>
           <Card.Content>
             <Text>{destination.description}</Text>
           </Card.Content>
