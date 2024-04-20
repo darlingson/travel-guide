4c3b394f7d0236ced934c7b9bcdef55e16bef845
darlingson <dmakuwila@gmail.com>
2024-04-20 20:50:37 +0200

    added the spinning indicator

--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView, View,ActivityIndicator } from "react-native";
 import { Card, Text } from "react-native-paper";
-
 const Recommendations = () => {
     const [recommendations, setRecommendations] = useState([]);
-
+    const [isLoading,setIsLoading] = useState(true);
     useEffect(() => {
       fetchRecommendations();
     }, []);
@@ -15,18 +14,22 @@ const Recommendations = () => {
         .then((data) => {
           console.log(data);
           setRecommendations(data.slice(0,5));
+          setIsLoading(false);
         })
         .catch((error) => {
           console.error(error);
+          setIsLoading(false);
         });
     };
   
     return (
         <ScrollView>
             <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 10 }}>Recommendations</Text>
+            {isLoading? <ActivityIndicator/>:
+            <>
             {recommendations.map((destination, index) => (
                 <Card key={destination.id}>
-                <Card.Title title={destination.name} />
+                <Card.Title title={destination.name} style={{ fontSize: 20, fontWeight: 'bold' }}/>
                 <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
                 </View>
                 <Card.Content>
@@ -34,6 +37,8 @@ const Recommendations = () => {
                 </Card.Content>
                 </Card>
             ))}
+            </>
+            }
         </ScrollView>
     );
 };
--- src/components/Spotlight.jsx
+++ src/components/Spotlight.jsx
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { ScrollView, View, Text, Image, Dimensions } from 'react-native';
-import { Button, Card } from 'react-native-paper';
+import { ActivityIndicator, Button, Card } from 'react-native-paper';
 
 const Spotlight = () => {
     const [spotlight, setSpotlight] = useState([]);
-
+    const [isLoading,setIsLoading] = useState(true);
     useEffect(() => {
         fetch('https://darlingson.pythonanywhere.com/destinations/spotlight')
             .then((response) => response.json())
             .then((data) => {
                 console.log(data);
                 setSpotlight(data);
+                setIsLoading(false);
             })
             .catch((error) => {
                 console.error(error);
+                setIsLoading(false);
             });
     },[]);
     return (
         <View>
         <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 10 }}>Spotlight</Text>
+        {isLoading ? <ActivityIndicator/>:
         <ScrollView contentContainerStyle={{ padding: 10 }}>
             <Card>
                 <Card.Title title={spotlight.name} />
@@ -32,6 +35,7 @@ const Spotlight = () => {
                 </Card.Content>
             </Card>
         </ScrollView>
+        }
         </View>
     );
 }
