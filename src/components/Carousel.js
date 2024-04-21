dbf4398c7bfb96a571d89ac3669f55dc875d35cc
darlingson <dmakuwila@gmail.com>
2024-04-21 18:35:51 +0200

    installed expo view pager

---
+++ src/components/Carousel.js
@@ -0,0 +1,3 @@
+import React,{useRef} from 'react';
+import { View, Text, Dimensions } from 'react-native';
+import CorouselComp from 'react-native-snap-carousel';
--- src/screens/DetailsScreen.jsx
+++ src/screens/DetailsScreen.jsx
@@ -1,15 +1,21 @@
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { Text } from "react-native-paper";
 import colors from "../../assets/styles/colors";
-
 function DetailsScreen({ route }) {
   const { destination } = route.params;
+  const baseUrl = 'https://darlingson.pythonanywhere.com/destination/image?path=';
+
+  // const processedImageUrls = destination.imageUrls.map((imageName) => {
+  //   return `${baseUrl}${imageName}`;
+  // });
+
+  let processedImageUrls = ["https://darlingson.pythonanywhere.com/destination/image?path=chingoni_2_by_unesco","https://darlingson.pythonanywhere.com/destination/image?path=chingoni_1_by_unesco"];
     return (
-      <View style={style.container}>
+      <ScrollView contentContainerStyle={style.container}>
         <Text>Details Screen</Text>
         <Text>Name: {destination.name}</Text>
         <Text>Description: {destination.description}</Text>
-      </View>
+      </ScrollView>
     );
   }
 const style = StyleSheet.create({
