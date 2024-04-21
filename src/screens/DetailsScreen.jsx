348882a089738f3cc012b2d245122a98fae486eb
darlingson <dmakuwila@gmail.com>
2024-04-21 17:37:18 +0200

    installed react native snap carouser

--- src/screens/DetailsScreen.jsx
+++ src/screens/DetailsScreen.jsx
@@ -1,12 +1,23 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Text } from "react-native-paper";
+import colors from "../../assets/styles/colors";
 
-function DetailsScreen() {
+function DetailsScreen({ route }) {
+  const { destination } = route.params;
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={style.container}>
         <Text>Details Screen</Text>
+        <Text>Name: {destination.name}</Text>
+        <Text>Description: {destination.description}</Text>
       </View>
     );
   }
-
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.primary,
+    alignItems: "left",
+    justifyContent: "left",
+  },
+});
   export default DetailsScreen
