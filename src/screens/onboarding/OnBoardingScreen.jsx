ce6b48e23fe4cae6c1ee1274d62165d56a85421d
darlingson <dmakuwila@gmail.com>
2024-04-22 23:10:18 +0200

    added features list to the onboarding screen

--- src/screens/onboarding/OnBoardingScreen.jsx
+++ src/screens/onboarding/OnBoardingScreen.jsx
@@ -4,10 +4,13 @@ import colors from "../../../assets/styles/colors";
 
 const OnBoardingScreen = ({ onNextClicked }) => {
     const features = [
-        "Feature 1: Lorem ipsum dolor sit amet",
-        "Feature 2: Consectetur adipiscing elit",
-        "Feature 3: Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-        // Add more features as needed
+        "Destination Discovery: Allow users to discover hidden gems",
+        "Destination Information: Give users detailed information about various destinations across Malawi, including popular attractions, historical sites, cultural landmarks, restaurants, accommodations, and shopping areas.",
+        "Offline Access: Download maps, guides, and other content for offline use, ensuring access even in areas with limited internet connectivity.",
+        "Trip Planning Tools: Plan itineraries, with suggestions for activities, dining options, accommodations, and transportation, tailored to suit the preferences and interests of users.",
+        "Weather Updates: Stay informed about the weather conditions in different parts of Malawi, helping you plan your activities and pack accordingly.",
+        "Currency Converter: Convenient currency converter tool for budgeting and making purchases.",
+        "Language Support: Translate phrases and expressions into local languages to enhance communication and interaction with fellow Malawians."
     ];
 
     return (
