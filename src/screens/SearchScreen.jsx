e38893c2c2ef7c8e1d4edd11d24ce23aa7133bea
darlingson <dmakuwila@gmail.com>
2024-04-25 20:21:46 +0200

    added the SearchScreenMainContent component

--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,14 +1,38 @@
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import colors from '../../assets/styles/colors';
+import { createStackNavigator } from '@react-navigation/stack';
+import DetailsScreen from './DetailsScreen';
+const Stack = createStackNavigator();
 
-const SearchScreen = () => (
-  <View style={styles.container}>
-    <Text>Home Screen</Text>
-  </View>
-);
 
+const SearchScreen = ({ navigation }) => {
+  // <View style={styles.container}>
+  //   <Text>Home Screen</Text>
+  // </View>
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Search Main Content'
+        component={SearchScreenMainContent}
+        options={{ headerShown: false }}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="DetailsScreen"
+        component={DetailsScreen}
+      ></Stack.Screen>
+    </Stack.Navigator>
+  );
+
+};
+const SearchScreenMainContent = () => {
+  return (
+    <View>
+      <Text>Main Content</Text>
+    </View>
+  );
+}
 const styles = StyleSheet.create({
-  container: {flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:colors.backgroundAlternative},
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: colors.backgroundAlternative },
 });
 
 export default SearchScreen;
